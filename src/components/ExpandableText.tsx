f75b2b175f6103ea9b0355022fd83d40118c7433
Junjie Chen <junjie.chan@outlook.com>
2024-10-30 20:47:09 +1000

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,36 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {
+  wordLimit?: number;
+  children: string;
+}
+
+const ExpandableText = ({ wordLimit = 100, children }: Props) => {
+  if (!children) return null;
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const textArray = children.split(" ");
+  const canExpand = textArray.length > wordLimit;
+  const text =
+    [...textArray].splice(0, wordLimit).join(" ") + (canExpand ? " ..." : "");
+
+  return (
+    <>
+      {isExpanded ? children : text}
+      {canExpand ? (
+        <Button
+          onClick={() => setIsExpanded(!isExpanded)}
+          size="xs"
+          fontWeight="bold"
+          colorScheme="yellow"
+          marginLeft={2}
+        >
+          {isExpanded ? "Show Less" : "Read More"}
+        </Button>
+      ) : null}
+    </>
+  );
+};
+
+export default ExpandableText;
