d296dc8f269be9f4ff7516f5946604cdc2a0fbda
Junjie Chen <junjie.chan@outlook.com>
2024-10-31 11:17:43 +1000

    Build game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,23 @@
+import { Image, SimpleGrid } from "@chakra-ui/react";
+import useScreenshots from "../hooks/useScreenshots";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (error) throw error;
+  if (isLoading) return null;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      {data?.results.map(({ image, id }) => (
+        <Image src={image} key={id} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
