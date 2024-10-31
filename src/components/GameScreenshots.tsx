1b58c34fc84e5a8652b530fd6eeda5f7804fb5b4
Junjie Chen <junjie.chan@outlook.com>
2024-10-31 13:45:32 +1000

    Add hover effect to game screenshots

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -14,7 +14,15 @@ const GameScreenshots = ({ gameId }: Props) => {
   return (
     <SimpleGrid columns={{ base: 1, md: 2 }} spacing={3} marginTop={5}>
       {data?.results.map(({ image, id }) => (
-        <Image src={image} key={id} borderRadius={8} />
+        <Image
+          src={image}
+          key={id}
+          borderRadius={8}
+          transition="all 0.15s ease-in-out"
+          _hover={{
+            transform: "scale(1.01)",
+          }}
+        />
       ))}
     </SimpleGrid>
   );
